6e4343837c497c71569f7a4620a2398004f458f4
Luke Carlson <jlukecarlson@gmail.com>
2016-10-02 16:54:05 -0400

    populated top urls page

--- client/top.js
+++ client/top.js
@@ -1,5 +1,11 @@
+var NUMBER_OF_TOP_URLS = 10;
+
 Template.top.helpers({
-    test: function () {
-        return Links.findOne();
+    topUrls: function () {
+        var urls = Links.find({},{
+            limit: NUMBER_OF_TOP_URLS,
+            sort: { views: -1 }
+        });
+        return urls
     }
 });
